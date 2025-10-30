a6258697fc442e5f7bf38daf29bb8b970a24a3a7
Aaditya Jain <155458768+AadityaJain0128@users.noreply.github.com>
2025-10-31 00:24:17 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-import subprocess as sp
+from backend.app import app
 
-# sp.run(["npm", "run", "build"], cwd=os.path.join(os.getcwd(), "frontend"), shell=True)
-sp.Popen(["serve", "dist"], cwd=os.path.join(os.getcwd(), "frontend"), shell=True)
-sp.Popen(["python", "app.py"], cwd=os.path.join(os.getcwd(), "backend"), shell=True)
+if __name__ == "__main__":
+  app.run()
